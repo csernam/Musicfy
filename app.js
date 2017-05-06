cc1e55f1011b643464875b2e357e2aaca5d43e24
BoK Geek <bok.geek@gmail.com>
2017-05-06 18:26:49 +0200

    remove test method

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //Load routes
+var user_routes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -13,8 +14,10 @@ app.use(bodyParser.json());
 //Headers config
 
 //Base routes
-app.get('/test', function(req, res){
-  res.status(200).send({message: 'Testing API...'});
-});
+app.use('/api', user_routes);
+
+// app.get('/test', function(req, res){
+//   res.status(200).send({message: 'Testing API...'});
+// });
 
 module.exports = app;
