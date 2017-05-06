69656b1927b74dfed1f2185ee851335d1136c3c1
BoK Geek <bok.geek@gmail.com>
2017-05-06 20:37:57 +0200

    get image from server

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
@@ -133,11 +135,25 @@ function uploadImages(req, res){
   }
 }
 
+function getImageFile(req, res){
+  var imageFile = req.params.imageFile;
+  var path_file = './uploads/users/'+imageFile;
+
+  fs.exists(path_file, function(exists){
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    }else{
+      res.status(200).send({message: 'Image not found'});
+    }
+  })
+}
+
 
 module.exports = {
   pruebas,
   saveUser,
   loginUser,
   updateUser,
-  uploadImages
+  uploadImages,
+  getImageFile
 };
--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,6 @@ api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImages);
-
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
 
 module.exports = api;
