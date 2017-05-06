de3a5c40c7d357c2320a704b3c2d3ab720a0605b
BoK Geek <bok.geek@gmail.com>
2017-05-06 17:34:31 +0200

    feat: run api server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
 
 mongoose.connect('mongodb://localhost:27017/musicfy', (err, res) => {
   if(err){
     throw err;
   }else{
     console.log("Musicfy connection is up and running...");
+
+    app.listen(port, function(){
+      console.log("API server running at http://localhost:" + port);
+    })
   }
 });
