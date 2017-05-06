5a0448d0f14abba2c4e2a9c7f7edcc19633b2c99
BoK Geek <bok.geek@gmail.com>
2017-05-06 18:29:00 +0200

    add user route

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var express = require('express');
+var UserController = require('../controllers/user');
+
+var api = express.Router();
+
+api.get('/probando', UserController.pruebas);
+api.post('/register', UserController.saveUser);
+
+module.exports = api;
