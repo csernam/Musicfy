99e97051f071faf35af3604275661ed8e58b17c3
BoK Geek <bok.geek@gmail.com>
2017-05-06 19:21:26 +0200

    (test): protect method with auth middleware

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
-api.get('/probando', UserController.pruebas);
+api.get('/probando', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
