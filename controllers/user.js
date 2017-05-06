e2fbc8a3351ae0e875fef0e7e901fd179d12748e
BoK Geek <bok.geek@gmail.com>
2017-05-06 19:57:37 +0200

    add user update controller

--- controllers/user.js
+++ controllers/user.js
@@ -86,8 +86,26 @@ function loginUser(req, res){
 
 }
 
+function updateUser(req, res){
+  var userId = req.params.id;
+  var update = req.body;
+
+  User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+    if(err){
+      res.status(500).send({message: 'Error: User could not be updated'});
+    }else{
+      if(!userUpdated){
+        res.status(404).send({message: 'User could not be updated'});
+      }else{
+        res.status(200).send({user: userUpdated});
+      }
+    }
+  });
+}
+
 module.exports = {
   pruebas,
   saveUser,
-  loginUser
+  loginUser,
+  updateUser
 };
--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/probando', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 
 module.exports = api;
