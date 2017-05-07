23e140033763435e14a751a037b90e20fdd8a8a1
BoK Geek <bok.geek@gmail.com>
2017-05-07 20:01:51 +0200

    add updateArtist

--- controllers/artist.js
+++ controllers/artist.js
@@ -72,9 +72,26 @@ function saveArtist(req, res){
   });
 }
 
+function updateArtist(req, res){
+  var artistId = req.params.id;
+  var update = req.body;
+
+  Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated) => {
+    if(err){
+      res.status(500).send({message: 'Error updating artist'});
+    }else{
+      if(!artistUpdated){
+        res.status(404).send({message: 'Artist not found'});
+      }else{
+        res.status(200).send({artist: artistUpdated});
+      }
+    }
+  });
+}
 
 module.exports = {
   getArtist,
   saveArtist,
-  getArtists
+  getArtists,
+  updateArtist
 }
--- routes/artist.js
+++ routes/artist.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
 api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
+api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
 
 module.exports = api;
