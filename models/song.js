4fffb60b9cd2b964197a80badf25cdd265b3dca1
BoK Geek <bok.geek@gmail.com>
2017-05-12 20:20:49 +0200

    fix: update song schema

--- models/song.js
+++ models/song.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var SongSchema = Schema({
   number: String,
   name: String,
-  duration: Number,
+  duration: String,
   file: String,
   album: { type: Schema.ObjectId, ref: 'Album' }
 });
