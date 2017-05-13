6af56618d471b3623eaf5e2e96a37022e081c707
BoK Geek <bok.geek@gmail.com>
2017-05-13 12:08:07 +0200

    feat: configure headers

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //Headers config
+app.use((req, res, next) => {
+  res.header('Access-Control.Allow-Origin', '*');
+  res.header('Access-Control.Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control.Access-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  next();
+})
 
 //Base routes
 app.use('/api', user_routes);
